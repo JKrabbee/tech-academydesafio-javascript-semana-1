43f2dc6603cdec095b18d26c51e838862be49f3c
JKrabbee <contatokrabbe@gmail.com>
2022-07-19 15:05:28 -0300

    Add functions (financas, formatarValor, exibirFinancas)

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,46 @@
+const financas = {
+  saldo: 10,
+  transacoes: [
+    {
+      descricao: 'Salgado na faculdade',
+      categoria: 'Despesa',
+      valor: 5.5
+    },
+    {
+      descricao: 'Livro Clean Code',
+      categoria: 'Despesa',
+      valor: 50
+    },
+    {
+      descricao: 'Grana do estágio',
+      categoria: 'Receita',
+      valor: 80
+    },
+    {
+      descricao: 'Capinha pro celular',
+      categoria: 'Despesa',
+      valor: 15
+    },
+  ]
+};
+
+function formatarValor(valor) {
+  return valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+}
+
+function exibirFinancas(){
+  console.log('saldo: ', formatarValor(financas.saldo));
+  financas.transacoes.forEach((transacao, i) => {
+    console.log('');
+
+    console.log('Transação', i + 1, ':')
+    console.log('[',transacao.categoria,']', transacao.descricao);
+    console.log('R$: ', transacao.valor);
+  })
+}
+
+exibirFinancas()
+
 function adicionarDespesa() {
   let descricaoDespesa = prompt("Qual a descricao de sua despesa?");
   let valorDespesa = prompt("Qual o valor de sua despesa?");
@@ -10,6 +53,17 @@ function adicionarDespesa() {
   if (isNaN(valorDespesa) == true) {
     alert("Por favor, digite um número válido");
   }
+
+  const valor = Number(valorDespesa);
+
+  const despesa = {
+    descricao: descricaoDespesa,
+    valor: valor,
+    categoria: 'Despesa'
+  }
+
+  financas.transacoes.push(despesa)
+  financas.saldo = financas.saldo - valor
 }
 function adicionarReceita() {
   let descricaoReceita = prompt("Qual a descricao de sua receita?");
@@ -22,6 +76,15 @@ function adicionarReceita() {
   if (isNaN(valorReceita) == true) {
     alert("Por favor, digite um número válido");
   }
-}
 
-adicionarDespesa();
+  const valor = Number(valorReceita);
+
+  const receita = {
+    descricao: descricaoReceita,
+    valor: valor,
+    categoria: 'Receita'
+  }
+
+  financas.transacoes.push(receita)
+  financas.saldo = financas.saldo + valor
+}
